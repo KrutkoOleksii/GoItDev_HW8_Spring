485000d659b3057e59001933f31a810a1a444dc7
oleksiikrutko <krutko.av@gmail.com>
2021-11-06 00:34:07 +0200

    deleted RoleController.java and RoleRepository.java

--- src/main/java/ua/goit/hw8Spring/repository/RoleRepository.java
+++
@@ -1,7 +0,0 @@
-//package ua.goit.hw8Spring.repository;
-//
-//import org.springframework.data.jpa.repository.JpaRepository;
-//import ua.goit.hw8Spring.model.Role;
-//
-//public interface RoleRepository extends JpaRepository<Role,Long> {
-//}
